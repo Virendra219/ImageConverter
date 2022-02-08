0e58b595dfb3329ac90e5299108aad280027d794
Virendra219 <virendrasn.min18@itbhu.ac.in>
2022-02-08 13:43:26 +0530

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace GreyToColor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Save_Button = new System.Windows.Forms.Button();
+            this.Edit_Button = new System.Windows.Forms.Button();
+            this.Show_Button = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.originalImgTab = new System.Windows.Forms.TabPage();
+            this.editedImgTab = new System.Windows.Forms.TabPage();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85.125F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.875F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(806, 451);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Save_Button);
+            this.groupBox1.Controls.Add(this.Edit_Button);
+            this.groupBox1.Controls.Add(this.Show_Button);
+            this.groupBox1.Location = new System.Drawing.Point(689, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 172);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operations";
+            // 
+            // Save_Button
+            // 
+            this.Save_Button.Location = new System.Drawing.Point(6, 127);
+            this.Save_Button.Name = "Save_Button";
+            this.Save_Button.Size = new System.Drawing.Size(94, 29);
+            this.Save_Button.TabIndex = 3;
+            this.Save_Button.Text = "Save As";
+            this.Save_Button.UseVisualStyleBackColor = true;
+            this.Save_Button.Click += new System.EventHandler(this.Save_Button_Click);
+            // 
+            // Edit_Button
+            // 
+            this.Edit_Button.Location = new System.Drawing.Point(6, 77);
+            this.Edit_Button.Name = "Edit_Button";
+            this.Edit_Button.Size = new System.Drawing.Size(94, 29);
+            this.Edit_Button.TabIndex = 2;
+            this.Edit_Button.Text = "Edit";
+            this.Edit_Button.UseVisualStyleBackColor = true;
+            this.Edit_Button.Click += new System.EventHandler(this.Edit_Button_Click);
+            // 
+            // Show_Button
+            // 
+            this.Show_Button.Location = new System.Drawing.Point(6, 29);
+            this.Show_Button.Name = "Show_Button";
+            this.Show_Button.Size = new System.Drawing.Size(94, 29);
+            this.Show_Button.TabIndex = 1;
+            this.Show_Button.Text = "Show";
+            this.Show_Button.UseVisualStyleBackColor = true;
+            this.Show_Button.Click += new System.EventHandler(this.Show_Button_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.originalImgTab);
+            this.tabControl1.Controls.Add(this.editedImgTab);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(680, 445);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // originalImgTab
+            // 
+            this.originalImgTab.Location = new System.Drawing.Point(4, 29);
+            this.originalImgTab.Name = "originalImgTab";
+            this.originalImgTab.Padding = new System.Windows.Forms.Padding(3);
+            this.originalImgTab.Size = new System.Drawing.Size(672, 412);
+            this.originalImgTab.TabIndex = 0;
+            this.originalImgTab.Text = "Original Image";
+            this.originalImgTab.UseVisualStyleBackColor = true;
+            // 
+            // editedImgTab
+            // 
+            this.editedImgTab.Location = new System.Drawing.Point(4, 29);
+            this.editedImgTab.Name = "editedImgTab";
+            this.editedImgTab.Padding = new System.Windows.Forms.Padding(3);
+            this.editedImgTab.Size = new System.Drawing.Size(672, 412);
+            this.editedImgTab.TabIndex = 1;
+            this.editedImgTab.Text = "Edited Image";
+            this.editedImgTab.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 451);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage originalImgTab;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Save_Button;
+        private System.Windows.Forms.Button Edit_Button;
+        private System.Windows.Forms.Button Show_Button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabPage editedImgTab;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreyToColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            originalImgTab.Controls.Add(originalImage);
+            originalImage.SizeMode = PictureBoxSizeMode.Zoom;
+            originalImage.Dock = DockStyle.Fill;
+
+            editedImgTab.Controls.Add(editedImage);
+            editedImage.SizeMode = PictureBoxSizeMode.Zoom;
+            editedImage.Dock = DockStyle.Fill;
+        }
+
+        PictureBox originalImage = new(), editedImage = new();
+        Bitmap BMOrg, BMEdit;
+
+        private void Show_Button_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                originalImage.Load(openFileDialog1.FileName);
+                tabControl1.SelectedTab = originalImgTab;
+            }
+        }
+
+        private void Edit_Button_Click(object sender, EventArgs e)
+        {
+            BMOrg = new(originalImage.Image);
+            int w = BMOrg.Width, h = BMOrg.Height;
+            BMEdit = new(w, h);
+
+            for (int i=0; i<h; i++)
+            {
+                for (int j=0; j<w; j++)
+                {
+                    BMEdit.SetPixel(j, i, BMOrg.GetPixel(j, i)); //customise here
+                }
+            }
+
+            editedImage.Image = BMEdit;
+            tabControl1.SelectedTab = editedImgTab;
+        }
+
+        private void Save_Button_Click(object sender, EventArgs e)
+        {
+            string fileAddress;
+            saveFileDialog1.Filter = "Bitmap Image (.bmp)|*.bmp|Gif Image (.gif)|*.gif|JPEG Image (.jpeg)|*.jpeg|Png Image (.png)|*.png|Tiff Image (.tiff)|*.tiff|Wmf Image (.wmf)|*.wmf";
+            try
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileAddress = saveFileDialog1.FileName;
+                    BMEdit.Save(fileAddress);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
